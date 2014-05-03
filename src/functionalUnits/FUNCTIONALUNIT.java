1bf64046ce89c29804d064661c0284036e975392
Ratnadeep Mangalvedhekar <rm7@umbc.edu>
2014-05-02 23:51:57 -0400

    ADDED - 1. Getters and Setters added to FUNCTIONALUNIT.java

--- src/functionalUnits/FUNCTIONALUNIT.java
+++ src/functionalUnits/FUNCTIONALUNIT.java
@@ -6,12 +6,48 @@ import java.util.Queue;
 
 public abstract class FUNCTIONALUNIT {
 	
-	boolean isPipelined;
-	boolean isAvailable;
-	int clockCyclesRequired;
-	Queue<Instruction> instructionQueue;
+	private boolean isPipelined;
+	private boolean isAvailable;
+	private int clockCyclesRequired;
+	private Queue<Instruction> instructionQueue;
 
+	
+	
+	public boolean isPipelined() {
+		return isPipelined;
+	}
+	
+	public void setPipelined(boolean isPipelined) {
+		this.isPipelined = isPipelined;
+	}
+	
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+	
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+	
+	public int getClockCyclesRequired() {
+		return clockCyclesRequired;
+	}
+	
+	public void setClockCyclesRequired(int clockCyclesRequired) {
+		this.clockCyclesRequired = clockCyclesRequired;
+	}
+	
+	public Queue<Instruction> getInstructionQueue() {
+		return instructionQueue;
+	}
+	
+	public void setInstructionQueue(Queue<Instruction> instructionQueue) {
+		this.instructionQueue = instructionQueue;
+	}
+	
 	public abstract void addInstructionToQueue(Instruction instruction);
 	public abstract Instruction removeInstructionFromQueue();
+	
+	
 
 }
