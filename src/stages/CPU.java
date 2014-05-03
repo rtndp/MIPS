36440172e74c01c8196c34d6d15cf1d695d01956
Ratnadeep Mangalvedhekar <rm7@umbc.edu>
2014-05-03 17:17:39 -0400

    ADDED - 1. CPU.java to Stages package

---
+++ src/stages/CPU.java
@@ -0,0 +1,10 @@
+package stages;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CPU
+{
+    public static int CLOCK           = 0;
+
+    public static int PROGRAM_COUNTER = 0;
+}
