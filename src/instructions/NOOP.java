26230eed66f3b26966e2717a31376d45a74369f8
Ratnadeep Mangalvedhekar <rm7@umbc.edu>
2014-05-02 23:11:17 -0400

    ADDED - 1. NOOP instruction added

---
+++ src/instructions/NOOP.java
@@ -0,0 +1,37 @@
+package instructions;
+
+import java.util.List;
+
+public class NOOP extends Instruction{
+
+	@Override
+	public List<String> getSourceRegister() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDestinationRegister() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void executeInstruction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void decodeInstruction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void writeBackResult() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
