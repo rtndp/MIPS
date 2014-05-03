c5ca623e3b6ce1c653f447346aed7b08167fc31f
Ratnadeep Mangalvedhekar <rm7@umbc.edu>
2014-05-03 02:34:21 -0400

    ADDED - 1. outline of logic for execute(), including peek(), tiebreaker();

--- src/stages/EX.java
+++ src/stages/EX.java
@@ -1,10 +1,59 @@
 package stages;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import functionalUnits.FPADD;
+import functionalUnits.FPDIV;
+import functionalUnits.FPMUL;
+import functionalUnits.IU;
+import functionalUnits.MEM;
+
 public class EX extends STAGE {
 
 	@Override
 	public void execute() {
 		
+		List<String> functionalUnits = new ArrayList<String>();
+		
+		List<String> winnerFunctionalUnit = tiebreaker(functionalUnits);
+		
+		System.out.println(winnerFunctionalUnit);
+		
+		if(peek().size() <= 1){
+				
+			FPADD fpadd = new FPADD();
+			fpadd.fpadd();
+			
+			FPMUL fpmul = new FPMUL();
+			fpmul.fpmul();
+			
+			FPDIV fpdiv = new FPDIV();
+			fpdiv.fpdiv();
+			
+			IU iu = new IU();
+			iu.iu();
+			
+			MEM mem = new MEM();
+			mem.mem();
+		
+		}else{
+			tiebreaker(peek());
+			//run all the funtional units except the loser(s) of tiebreaker()
+		}
+		
+		
+	}
+	
+	private List<String> tiebreaker(List<String> functionalUnits){
+		
+		return null;
+	}
+	
+	private List<String> peek(){
+		List<String> temp = new ArrayList<String>();
+		
+		return temp;
 	}
 
 }
