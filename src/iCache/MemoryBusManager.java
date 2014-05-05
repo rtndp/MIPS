aae9d28b978c6a5a025e5a167f94986f2239b691
Ratnadeep Mangalvedhekar <rm7@umbc.edu>
2014-05-05 04:05:14 -0400

    Added - 1. Created a package memoryBus 2. Added a MemoryBusManager.java in the new package

--- src/iCache/MemoryBusManager.java
+++ src/iCache/MemoryBusManager.java
@@ -1,4 +1,4 @@
-package cache;
+package iCache;
 
 public class MemoryBusManager
 {
---
+++ src/memoryBus/MemoryBusManager.java
@@ -0,0 +1,26 @@
+package memoryBus;
+
+public class MemoryBusManager
+{
+
+	private static volatile MemoryBusManager instance;
+
+	public static MemoryBusManager getInstance() {
+		if (null == instance)
+			synchronized (MemoryBusManager.class) {
+				if (null == instance)
+					instance = new MemoryBusManager();
+			}
+
+		return instance;
+	}
+	
+	
+	public int getDelayFromBCM(){
+		
+		return 0;
+		
+	}
+	
+	
+}
