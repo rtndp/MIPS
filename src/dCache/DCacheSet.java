5d23bc807d163f04cef0a560a7a66b573e0f7030
Ratnadeep Mangalvedhekar <rm7@umbc.edu>
2014-05-05 04:15:21 -0400

    ADDED - 1. Constructors add/singleton implementation added for all the classes

--- src/dCache/DCacheSet.java
+++ src/dCache/DCacheSet.java
@@ -1,5 +1,17 @@
 package dCache;
 
 public class DCacheSet {
+	DCacheBlock[] dCacheBlocks;
+	int lru;
 
+	public static final DCacheSet instance = new DCacheSet();
+
+	private DCacheSet() {
+
+		dCacheBlocks = new DCacheBlock[2];
+		for (int i = 0; i < 2; i++) {
+			dCacheBlocks[i] = new DCacheBlock();
+		}
+
+	}
 }
