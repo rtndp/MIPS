429b94fbaf65c22a106d30cc6f9642684855496b
Ratnadeep Mangalvedhekar <rm7@umbc.edu>
2014-05-05 03:50:03 -0400

    ADDED - 1. Switch case in main for With Memory Hierarchy and without Memory Hierarchy

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import cache.ICacheManager;
 import instructions.Instruction;
 import memory.DataMemoryFileParser;
 import memory.DataMemoryManager;
@@ -17,109 +18,101 @@ import stages.WriteBackStage;
 import config.ConfigManager;
 import config.ConfigParser;
 
-public class Main
-{
-    /**
-     * 
-     * @param args
-     *            inst.txt data.txt reg.txt config.txt result.txt
-     * @throws Exception
-     */
-    public static void main(String[] args) throws Exception
-    {
+public class Main {
+	/**
+	 * 
+	 * @param args
+	 *            inst.txt data.txt reg.txt config.txt result.txt
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
 
-        ProgramParser.parse(args[0]);
-        ProgramManager.instance.dumpProgram();
+		ProgramParser.parse(args[0]);
+		ProgramManager.instance.dumpProgram();
 
-        DataMemoryFileParser.parseMemoryFile(args[1]);
-        DataMemoryManager.instance.dumpAllMemory();
+		DataMemoryFileParser.parseMemoryFile(args[1]);
+		DataMemoryManager.instance.dumpAllMemory();
 
-        RegisterFileParser.parseRegister(args[2]);
-        RegisterManager.instance.dumpAllRegisters();
+		RegisterFileParser.parseRegister(args[2]);
+		RegisterManager.instance.dumpAllRegisters();
 
-        ConfigParser.parseConfigFile(args[3]);
-        ConfigManager.instance.dumpConfiguration();
+		ConfigParser.parseConfigFile(args[3]);
+		ConfigManager.instance.dumpConfiguration();
 
-        ResultsManager.instance.setResultsPath(args[4]);
+		ResultsManager.instance.setResultsPath(args[4]);
 
-        /**
-         * Initialize Global CLOCK and PC to 0
-         */
-        CPU.CLOCK = 0;
-        CPU.PROGRAM_COUNTER = 0;
-        CPU.RUN_TYPE = RUN.PIPELINE;
+		/**
+		 * Initialize Global CLOCK and PC to 0
+		 */
+		CPU.CLOCK = 0;
+		CPU.PROGRAM_COUNTER = 0;
+		CPU.RUN_TYPE = RUN.PIPELINE;
+		//CPU.RUN_TYPE = RUN.MEMORY;
 
-        WriteBackStage wbStage = WriteBackStage.getInstance();
-        ExStage exStage = ExStage.getInstance();
-        DecodeStage idStage = DecodeStage.getInstance();
-        FetchStage ifStage = FetchStage.getInstance();
+		WriteBackStage wbStage = WriteBackStage.getInstance();
+		ExStage exStage = ExStage.getInstance();
+		DecodeStage idStage = DecodeStage.getInstance();
+		FetchStage ifStage = FetchStage.getInstance();
 
-        try
-        {
+		try {
 
-            int extraCLKCount = 5000; // Extra number of CLOCK cycles that will
-                                      // be run after HLT is Decoded
-            while (extraCLKCount != 0)
-            {
+			int extraCLKCount = 5000; // Extra number of CLOCK cycles that will
+										// be run after HLT is Decoded
+			while (extraCLKCount != 0) {
 
-                wbStage.execute();
-                exStage.execute();
+				wbStage.execute();
+				exStage.execute();
 
-                // Well this is just stupid way of doing this
-                if (!ResultsManager.instance.isHALT())
-                {
-                    idStage.execute();
+				// Well this is just stupid way of doing this
+				if (!ResultsManager.instance.isHALT()) {
+					idStage.execute();
 
-                    if (!ResultsManager.instance.isHALT())
-                    {
-                        ifStage.execute();
+					if (!ResultsManager.instance.isHALT()) {
+						ifStage.execute();
 
-                        // fetch a new instruction only if ifStage is free
-                        if (ifStage.checkIfFree())
-                        {
-                            boolean checkInst = false;
+						// fetch a new instruction only if ifStage is free
+						if (ifStage.checkIfFree()) {
+							boolean checkInst = false;
 
-                            Instruction next = null;
-                            switch (CPU.RUN_TYPE)
-                            {
-                                case MEMORY:
-                                    break;
+							Instruction next = null;
+							switch (CPU.RUN_TYPE) {
+							case MEMORY:
+								next = ICacheManager.getInstance()
+										.getInstructionFromCache(
+												CPU.PROGRAM_COUNTER);
+								if (next != null)
+									checkInst = true;
+									break;
 
-                                case PIPELINE:
-                                    next = ProgramManager.instance
-                                            .getInstructionAtAddress(CPU.PROGRAM_COUNTER);
-                                    checkInst = true;
-                                    break;
-                            }
+							case PIPELINE:
+								next = ProgramManager.instance
+										.getInstructionAtAddress(CPU.PROGRAM_COUNTER);
+								checkInst = true;
+								break;
+							}
 
-                            if (checkInst && ifStage.checkIfFree()
-                                    && ifStage.acceptInstruction(next))
-                            {
-                                // update entry clock?
-                                CPU.PROGRAM_COUNTER++;
-                            }
+							if (checkInst && ifStage.checkIfFree()
+									&& ifStage.acceptInstruction(next)) {
+								// update entry clock?
+								CPU.PROGRAM_COUNTER++;
+							}
 
-                        } // end ifStage.checkIfFree
-                    }
-                }
-                else
-                    extraCLKCount--;
+						} // end ifStage.checkIfFree
+					}
+				} else
+					extraCLKCount--;
 
-                CPU.CLOCK++;
-            }
-        }
-        catch (Exception e)
-        {
-            System.out.println("ERROR: CLOCK=" + CPU.CLOCK);
-            e.printStackTrace();
-        }
-        finally
-        {
-        }
-        Thread.sleep(1000L);
-        System.out.println("Results");
-        ResultsManager.instance.printResults();
-        ResultsManager.instance.writeResults();
+				CPU.CLOCK++;
+			}
+		} catch (Exception e) {
+			System.out.println("ERROR: CLOCK=" + CPU.CLOCK);
+			e.printStackTrace();
+		} finally {
+		}
+		Thread.sleep(1000L);
+		System.out.println("Results");
+		ResultsManager.instance.printResults();
+		ResultsManager.instance.writeResults();
 
-    }
+	}
 }
